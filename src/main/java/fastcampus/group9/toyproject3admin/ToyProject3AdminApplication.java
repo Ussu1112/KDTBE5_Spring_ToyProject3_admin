138f2a8d6ebde7d8b84e2fcd695d2a217411db61
ussu1112 <ussu1112@naver.com>
2023-07-19 13:31:43 +0900

    chore: 빌드 패키지 수정 프로젝트 세팅

---
+++ src/main/java/fastcampus/group9/toyproject3admin/ToyProject3AdminApplication.java
@@ -0,0 +1,13 @@
+package fastcampus.group9.toyproject3admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProject3AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyProject3AdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fastcampus/group9/toyproject3admin/ToyProject3AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package fastcampus.group9.toyproject3admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject3AdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
